4496505f606a9ee662d6f0d88eec388c94b52db3
yc850k <yc850k@qq.com>
2015-09-10 11:51:28 +0800

    add default expire in etcd-task

--- etcd-task/register.go
+++ etcd-task/register.go
@@ -10,13 +10,22 @@ const (
 )
 
 //register or update
-func Register(name string, task *Task) error {
+func Register(name string, task *Task, params ...uint64) error {
+	var expire uint64 = 3 * HEARTBEAT_DURATION
+	if len(params) > 0 {
+		expire = params[0]
+	}
+	err := register(name, task, expire)
+	return err
+}
+
+func register(name string, task *Task, expire uint64) error {
 	key := fmt.Sprintf("/tasks/%s/%s", name, task.Name)
 	taskJson, err := json.Marshal(&task)
 	if err != nil {
 		return err
 	}
 	value := string(taskJson)
-	_, err = Client().Set(key, value, 3*HEARTBEAT_DURATION)
+	_, err = Client().Set(key, value, expire)
 	return err
 }
