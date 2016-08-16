2f070629b2e68850f14fdcef542d2031f7597f2a
yc850k <yc850k@qq.com>
2016-08-16 12:09:26 +0800

    use ETCD_HOSTS first

--- etcd-task/init.go
+++ etcd-task/init.go
@@ -17,25 +17,38 @@ var (
 
 func Client() *etcd.Client {
 	clientSingletonO.Do(func() {
-		host := "http://localhost:4001"
-		if len(os.Getenv("ETCD_HOST")) != 0 {
-			host = os.Getenv("ETCD_HOST")
+		hosts := []string{"http://localhost:4001"}
+		if len(os.Getenv("ETCD_HOSTS")) != 0 {
+			strhosts := os.Getenv("ETCD_HOSTS")
+			hosts = strings.Split(strhosts, ",")
 		}
 
 		cacert := os.Getenv("ETCD_CACERT")
 		tlskey := os.Getenv("ETCD_TLS_KEY")
 		tlscert := os.Getenv("ETCD_TLS_CERT")
+
+		ishttps := false
 		if len(cacert) != 0 && len(tlskey) != 0 && len(tlscert) != 0 {
-			if !strings.Contains(host, "https://") {
-				host = strings.Replace(host, "http", "https", 1)
+			ishttps = true
+		}
+
+		etcdhosts := make([]string, len(hosts))
+		for _, host := range hosts {
+			if ishttps {
+				if !strings.Contains(host, "https://") {
+					host = strings.Replace(host, "http", "https", 1)
+				}
 			}
-			c, err := etcd.NewTLSClient([]string{host}, tlscert, tlskey, cacert)
+			etcdhosts = append(etcdhosts, host)
+		}
+		if ishttps {
+			c, err := etcd.NewTLSClient(etcdhosts, tlscert, tlskey, cacert)
 			if err != nil {
 				panic(err)
 			}
 			clientSingleton = c
 		} else {
-			clientSingleton = etcd.NewClient([]string{host})
+			clientSingleton = etcd.NewClient(etcdhosts)
 		}
 	})
 	return clientSingleton
